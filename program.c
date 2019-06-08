d150d50fea9b842dae024d369f8e623973cfd6ce
cse <cse93@hansung.ac.kr>
2019-06-09 03:51:37 +0900

    added program.c

---
+++ program.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int fib(int n)
+{
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  return fib(n-1) + fib(n-2);
+}
+
+int sum(int a,int b)
+{
+    return a+b;
+}
+
+int main()
+{
+  printf("Hello world!\n");
+  printf("%d\n", fib(5));
+  printf("%d + %d = %d",1,2,sum(1,2));
+  return 0;
+}
+
